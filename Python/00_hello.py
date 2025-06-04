a130e561482663f58eadaa1db9357e6e047bded2
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-06-04 14:48:31 +0200

    cambios

---
+++ Python/00_hello.py
@@ -0,0 +1,16 @@
+# Hola mundo
+print("Hola Python, soy José Antonio Romero Pérez")
+
+"""
+Este es un comentario para varias lineas y como se realiza, se aconseja dejar comentarios cuando se aprende
+
+"""
+# Print como cadena de texto
+print('Hola Python, te saludo desde otra forma, pero sigo siendo José Antonio Romero Pérez')
+
+# Descripción de las funciones con Type
+print(type("Hola Python, soy del tipo:")) # Tipo "str" es decir texto
+print(type(5)) # Tipo "int" es decir entero
+print(type(5.0)) # Tipo "float" es decir decimal
+print(type(True)) # Tipo "bool" es decir booleano
+print(type(3+1j)) # Tipo "complex" es decir complejo
