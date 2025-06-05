f2bd0694bc1c387c926eab014b2d78533f91f01f
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-06-05 14:50:26 +0200

    Modificaciones

---
+++ Python/02_operators.py
@@ -0,0 +1,38 @@
+### Operadores en Python ###
+
+print( 3 + 6) # Operador de suma
+print( 3 - 6) # Operador de resta
+print( 3 * 6) # Operador de multiplicación
+print( 3 / 6) # Operador de división
+print( 10 % 6) # Operador de módulo
+print( 10 // 6) # Operador de división entera
+print( 10 ** 6) # Operador de potencia  
+
+print("Hola" + "Python" + "¿Qué vas a calcular hoy?")
+print("Hola" + str(4))
+print("Hola" * 4)
+print("Hola" * (2 ** 2))
+
+
+my_float = 3.14 * 5
+print("Hola " * int(my_float))
+
+# Operadores de comparación
+
+print(2 > 4) # Mayor que
+print(2 < 4) # Menor que
+print(2 >= 4) # Mayor o igual que
+print(2 <= 4) # Menor o igual que
+print(2 == 4) # Igual que
+print(2 != 4) # Diferente que
+
+x= 5  #Modo matematico para resolver una incognita (Los operadores puede usarse también para ver si está en orden una lista por letras o números)
+print(x < 4)
+print(x > 4)
+
+# Operadores Lógicos (Booleanas)
+
+print(True and True) # AND
+print(True or False) # OR  
+print(not True) # NOT  
+print (not(True)) # NOT con paréntesis
