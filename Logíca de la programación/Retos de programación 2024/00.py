ee5e3ad0c450237647fde77bbe9852362c942df2
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-06-20 20:44:55 +0200

    comenzando a picar

---
+++ Logíca de la programación/Retos de programación 2024/00.py
@@ -0,0 +1,45 @@
+""""
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+"""
+
+# La página web que he elegido es Python y la pagina es https://python.org
+
+# Comentar en una linea
+
+"""
+Comentar en varios lineas, por lo que no afecta a la ejecución
+"""
+
+'''
+
+Esto es otra forma de comentar en varios lineas
+
+'''
+
+x = "mi variable" # Forma matemática
+
+my_variable = "Mi variable" # Cadena de texto 
+
+# Python no tiene constante
+
+MY_CONSTANT =   "Mi constante" # por convención 
