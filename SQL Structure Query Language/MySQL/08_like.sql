148ea03d9f32861a2c7c8513ceaf24cb1f1d80fd
José Antonio Romero Pérez <jarptgd@gmail.com>
2025-07-10 12:38:33 +0200

    modificaciones

---
+++ SQL Structure Query Language/MySQL/08_like.sql
@@ -0,0 +1,5 @@
+SELECT * FROM mi_primeradbmysql.users WHERE email LIKE '%gmail.com';
+
+SELECT * FROM mi_primeradbmysql.users WHERE email LIKE 'jarp%';
+
+SELECT * FROM mi_primeradbmysql.users WHERE email LIKE '%@%'
---
+++ SQL Structure Query Language/MySQL/09_not.sql
@@ -0,0 +1,5 @@
+SELECT * FROM mi_primeradbmysql.users WHERE NOT email ='jarple90@gmail.com';
+
+SELECT * FROM mi_primeradbmysql.users WHERE NOT email ='jarple90@gmail.com' AND age= 14;
+
+SELECT * FROM mi_primeradbmysql.users WHERE NOT email = 'jarple90@gmail.com' OR age= 14;
